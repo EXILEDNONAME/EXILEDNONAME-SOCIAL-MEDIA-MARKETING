8668d1d40d2894d153159b36cbe6b9f0c921cc6f
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-07-29 20:33:47 +0700

    9

--- resources/views/pages/frontend/index.blade.php
+++ resources/views/pages/frontend/index.blade.php
@@ -1,68 +1,265 @@
 <!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="author" content="EXILEDNONAME">
-  <meta name="description" content="EXILEDNONAME">
-  <meta name="keywords" content="EXILEDNONAME">
-  <title>
-    @php $title = DB::table('system_settings')->first(); @endphp
-    {{ $title->application_name; }}
-   </title>
-  <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/frontend/font-awesome.css">
-  <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/frontend/css/animate.css">
-  <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/frontend/css/star-animation.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/frontend/css/reset.css">
-  <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/frontend/css/structure.css">
-  <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/frontend/css/main-style.css">
-  <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/frontend/css/responsive.css">
-  <link rel="shortcut icon" href="{{ env('APP_URL') }}/assets/favicon.png"/>
-</head>
-<body class="single-image star-animation">
-  <div class="page-loader-wrapper">
-    <div class="loader"></div>
-  </div>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="">
+    <meta name="description" content="EXILEDNONAME - Social Media Marketing Digital">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,700,900" rel="stylesheet">
+    <title> EXILEDNONAME - Social Media Marketing </title>
+    <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/frontend/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/frontend/css/font-awesome.css">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/assets/frontend/css/templatemo-softy-pinko.css">
+    <link rel="shortcut icon" href="{{ env('APP_URL') }}/assets/frontend/logo.png"/>
+    <style>
+      .whatsapp-button{
+        position: fixed;
+        bottom: 15px;
+        right: 15px;
+        z-index: 99;
+        background-color: #25d366;
+        border-radius: 50px;
+        color: #ffffff;
+        text-decoration: none;
+        width: 50px;
+        height: 50px;
+        font-size: 30px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        -webkit-box-shadow: 0px 0px 25px -6px rgba(0,0,0,1);
+        -moz-box-shadow: 0px 0px 25px -6px rgba(0,0,0,1);
+        box-shadow: 0px 0px 25px -6px rgba(0,0,0,1);
+        animation: effect 5s infinite ease-in;
+      }
 
-  <div id="main-wrapper">
-    <main id="main" class="background" data-image="{{ env('APP_URL') }}/assets/frontend/images/background.jpg">
+      @keyframes effect {
+        20%, 100% {
+          width: 50px;
+          height: 50px;
+          font-size: 30px;
+        }
+        0%, 10%{
+          width: 55px;
+          height: 55px;
+          font-size: 35px;
+        }
+        5%{
+          width: 50px;
+          height: 50px;
+          font-size: 30px;
+        }
+      }
+    </style>
+  </head>
+  <body>
 
-      <div class="bg-animation">
-        <div id='stars'></div>
-        <div id='stars2'></div>
-        <div id='stars3'></div>
-        <div id='stars4'></div>
-      </div>
 
-      <div class="overlay" style="background-color: rgba(0,0,0,0.3)"></div>
-      <div class="nc-content-section index nc-active vhm">
-        <div id="home-page" class="page-wrapper vhm-item active-home anim s01">
-          <div class="container">
-            <div class="tagline ac anim fadeInUp s01 delay-0-6s">
-              <h1> {{ $title->application_name; }} </h1>
-              <p class="anim fadeInUp s01 delay-0-7s"> Our Website Is Coming Soon. We`ll be here soon with our new Imagination. </p>
-            </div>
 
-            <hr>
-
-          <div class="copyrights ac anim fadeInUp s01 delay-15s">
-
-          <br>
-          @2025 - @php $title = DB::table('system_settings')->first(); @endphp
-          {{ $title->application_name; }}
-        </div>
-        </div>
+    <div id="preloader">
+      <div class="jumper">
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
 
-  </main>
-</div>
+    <header class="header-area header-sticky">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <nav class="main-nav">
+              <a href="#" class="logo">
+                <h4 class="text-dark font-weight-bold"> EXILEDNONAME </h4>
+              </a>
+              <ul class="nav">
+                <li><a href="#home" class="active">Home</a></li>
+                <li><a href="#harga"> Harga </a></li>
+                <li><a href="/login"> Login </a></li>
+              </ul>
+              <a class='menu-trigger'>
+                <span>Menu</span>
+              </a>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
 
-<script src="{{ env('APP_URL') }}/assets/frontend/js/jquery-1.11.3.min.js"></script>
-<script src="{{ env('APP_URL') }}/assets/frontend/js/plugins.js"></script>
-<script src="{{ env('APP_URL') }}/assets/frontend/js/common.js"></script>
+    <div class="welcome-area" id="home">
+      <div class="header-text">
+        <div class="container">
+          <div class="row">
+            <div class="offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 col-md-12 col-sm-12">
+              <h1>Dapatkan lebih banyak <strong>Followers</strong>,
+                <strong>Likes</strong>, dan <strong>Views</strong>
+                dengan harga terjangkau bersama kami
+                <br><br>
+                <a href="/register" class="main-button-slider"> DAFTAR SEKARANG </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-</body>
-</html>
+      <section class="section home-feature">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="row">
+                <!-- ***** Features Small Item Start ***** -->
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s">
+                  <div class="features-small-item">
+                    <div class="icon">
+                      <i><img src="{{ env('APP_URL') }}/assets/frontend/images/image-01.png" width="75" height="75" alt=""></i>
+                    </div>
+                    <h5 class="features-title"> Layanan Berkualitas </h5>
+                    <p> Anda akan menemukan layanan berkualitas tinggi di Dashboard kami. </p>
+                  </div>
+                </div>
+                <!-- ***** Features Small Item End ***** -->
+
+                <!-- ***** Features Small Item Start ***** -->
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.4s">
+                  <div class="features-small-item">
+                    <div class="icon">
+                      <i><img src="{{ env('APP_URL') }}/assets/frontend/images/image-02.png" width="85" height="85" alt=""></i>
+                    </div>
+                    <h5 class="features-title">Harga Termurah</h5>
+                    <p> Nikmati Layanan terbaik dengan harga termurah </p>
+                  </div>
+                </div>
+                <!-- ***** Features Small Item End ***** -->
+
+                <!-- ***** Features Small Item Start ***** -->
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.6s">
+                  <div class="features-small-item">
+                    <div class="icon">
+                      <i><img src="{{ env('APP_URL') }}/assets/frontend/images/image-03.png" width="75" height="75" alt=""></i>
+                    </div>
+                    <h5 class="features-title">Proses Cepat</h5>
+                    <p> Semua pesanan masuk otomatis di proses secara instan </p>
+                  </div>
+                </div>
+                <!-- ***** Features Small Item End ***** -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section colored" id="harga">
+        <div class="container">
+          <!-- ***** Section Title Start ***** -->
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="center-heading">
+                <h2 class="section-title"> Harga </h2>
+              </div>
+            </div>
+            <div class="offset-lg-3 col-lg-6">
+              <div class="center-text">
+                <p> Kami menyediakan rekomendasi pilihan untuk anda </p>
+              </div>
+            </div>
+          </div>
+          <!-- ***** Section Title End ***** -->
+
+          <div class="row">
+            <!-- ***** Pricing Item Start ***** -->
+            <div class="col-lg-4 col-md-6 col-sm-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s">
+              <div class="pricing-item">
+                <div class="pricing-header">
+                  <h3 class="pricing-title">Tiktok Views </h3>
+                </div>
+                <div class="pricing-body">
+                  <div class="price-wrapper">
+                    <span class="price">Rp 0 </span>
+                  </div>
+                  <ul class="list">
+                    <li class="active"> Dapatkan lebih banyak Tiktok Views </li>
+                    <li class="active"> Syarat dan Ketentuan Berlaku </li>
+
+                  </ul>
+                </div>
+                <div class="pricing-footer">
+                  <a href="/register" class="main-button"> BELI </a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s">
+              <div class="pricing-item">
+                <div class="pricing-header">
+                  <h3 class="pricing-title"> Tiktok Likes </h3>
+                </div>
+                <div class="pricing-body">
+                  <div class="price-wrapper">
+                    <span class="price">Rp 0 </span>
+                  </div>
+                  <ul class="list">
+                    <li class="active"> Dapatkan lebih banyak Tiktok Likes </li>
+                    <li class="active"> Syarat dan Ketentuan Berlaku </li>
+                  </ul>
+                </div>
+                <div class="pricing-footer">
+                  <a href="/register" class="main-button"> BELI </a>
+                </div>
+              </div>
+            </div>
+            <!-- ***** Pricing Item End ***** -->
+
+            <!-- ***** Pricing Item Start ***** -->
+            <div class="col-lg-4 col-md-6 col-sm-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.4s">
+              <div class="pricing-item active">
+                <div class="pricing-header">
+                  <h3 class="pricing-title"> Instagram Likes</h3>
+                </div>
+                <div class="pricing-body">
+                  <div class="price-wrapper">
+                    <span class="price">Rp 0</span>
+                  </div>
+                  <ul class="list">
+                    <li class="active"> Dapatkan lebih banyak Instagram Likes </li>
+                    <li class="active"> Syarat dan Ketentuan Berlaku </li>
+
+                  </ul>
+                </div>
+                <div class="pricing-footer">
+                  <a href="/register" class="main-button"> BELI </a>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </section>
+
+      <footer>
+        <div class="container">
+
+          <div class="row">
+            <div class="col-lg-12">
+              <p class="copyright">Copyright &copy; 2025 - EXILEDNONAME</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+
+
+      <script src="{{ env('APP_URL') }}/assets/frontend/js/jquery-2.1.0.min.js"></script>
+      <script src="{{ env('APP_URL') }}/assets/frontend/js/popper.js"></script>
+      <script src="{{ env('APP_URL') }}/assets/frontend/js/bootstrap.min.js"></script>
+      <script src="{{ env('APP_URL') }}/assets/frontend/js/scrollreveal.min.js"></script>
+      <script src="{{ env('APP_URL') }}/assets/frontend/js/waypoints.min.js"></script>
+      <script src="{{ env('APP_URL') }}/assets/frontend/js/jquery.counterup.min.js"></script>
+      <script src="{{ env('APP_URL') }}/assets/frontend/js/imgfix.min.js"></script>
+      <script src="{{ env('APP_URL') }}/assets/frontend/js/custom.js"></script>
+
+      <a target="_blank" href="https://api.whatsapp.com/send?phone=6289672705020&text=Saya klik dari https://exilednoname.site, Mau tanya ...." class="whatsapp-button"><img src="https://www.freeiconspng.com/uploads/whatsapp-icon-png-13.png" height="50"></a>
+
+    </body>
+  </html>
